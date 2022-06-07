611c9ac0fdb91c1d9e04e602c8a85c6c2504e253
mellowo <mellowo@icloud.com>
2022-06-07 15:23:11 +0900

    junit을 활용한 test case 작성

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        //primitive type으로 해도 상관 없으나, null 을 사용할 수 없어 Wrapper 타입으로 진행
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
