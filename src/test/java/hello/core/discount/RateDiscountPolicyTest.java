1c9e978310038c8d733ae1aa0ec49d753a344dff
mellowo <mellowo@icloud.com>
2022-06-07 21:18:49 +0900

    OrderServiceImpl에서 FixDiscountPolicy -> RateDiscountPolicyTest으로 변경

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,4 +1,4 @@
-package hello.core.discount;
+ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
@@ -13,7 +14,7 @@ class RateDiscountPolicyTest {
 
     @Test
     @DisplayName("VIP는 10% 할인이 적용되어야 한다")
-    void vip_o(){
+    void vip_o() {
         //given
         Member member = new Member(1L, "memberVIP", Grade.VIP);
 
@@ -21,7 +22,19 @@ class RateDiscountPolicyTest {
         int discount = discountPolicy.discount(member, 10000);
 
         //then
-        Assertions.assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(1000);
     }
 
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void vip_x() {
+        //given
+        Member member = new Member(1L, "memberBasic", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
 }
