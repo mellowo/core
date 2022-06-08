b94cb540523c05fa7238892ec00f4c2e17e3e608
mellowo <mellowo@icloud.com>
2022-06-08 22:34:48 +0900

    Bean 객체 불러오기 test , 부모 자식간 Bean 객체 불러오기, Object 객체로 모든 bean 객체 불러오기

---
+++ src/main/java/hello/core/TestMain.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+class Test1{
+
+}
+
+public class TestMain {
+    public static void main(String[] args) {
+        Test1 t1 = new Test1();
+
+        System.out.println(Test1.class);
+    }
+}
