b0aaa3ea1c198d0c62b7e967556e17e4fba8a72d
mellowo <mellowo@icloud.com>
2022-06-09 11:06:37 +0900

    xml 방식의 DI study, Beandefinition study

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,27 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext applicationContext = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+
+
+
+
+
+
+}
