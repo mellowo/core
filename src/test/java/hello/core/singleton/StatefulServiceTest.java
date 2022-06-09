8e304f646f84db6eb5b3f8c9b2713f9a517252b5
mellowo <mellowo@icloud.com>
2022-06-09 14:40:59 +0900

    stateful의 문제점

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,35 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+
+    @Test
+    @DisplayName("Stateful")
+    void statefulServiceSingleton(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        statefulService1.order("userA", 10000);
+        statefulService2.order("userB", 20000);
+
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+    }
+
+
+    static class TestConfig{
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+}
