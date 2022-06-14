eac9a91bd6fe5f9632ef7c57a60d04ab2a1265e7
mellowo <mellowo@icloud.com>
2022-06-14 11:14:24 +0900

    Spring Bean 직접 만든 Bean만 조회 하기

--- src/test/java/hello/core/SpringBeanSearchTest.java
+++ src/test/java/hello/core/SpringBeanSearchTest.java
@@ -2,21 +2,36 @@ package hello.core;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.BeanDefinitionDsl;
 
 public class SpringBeanSearchTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
     @Test
     @DisplayName("모든 빈 출력하기")
-    void springBeanSearch(){
+    void springBeanSearch() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println("beanDefinitionName = " + beanDefinitionName);
             Object bean = ac.getBean(beanDefinitionName);
             System.out.println("bean = " + bean);
         }
+    }
 
+    @Test
+    @DisplayName("직접 생성한 빈만 출력하기")
+    void searchMyBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("bean = " + bean);
+            }
 
+        }
     }
 }
