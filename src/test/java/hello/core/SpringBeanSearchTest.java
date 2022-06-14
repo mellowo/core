7f611c7b7ee3c3f4370ce0f0055dc95ff707caa5
mellowo <mellowo@icloud.com>
2022-06-14 10:53:13 +0900

    Spring Bean 조회 test

---
+++ src/test/java/hello/core/SpringBeanSearchTest.java
@@ -0,0 +1,22 @@
+package hello.core;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringBeanSearchTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void springBeanSearch(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("beanDefinitionName = " + beanDefinitionName);
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("bean = " + bean);
+        }
+
+
+    }
+}
